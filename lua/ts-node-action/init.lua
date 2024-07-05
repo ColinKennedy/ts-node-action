83f0d802784380f2484269d1888e41ee5900119c
Colin Kennedy <colinvfx@gmail.com>
2024-07-04 22:34:14 -0700

    Added visual lines function

--- lua/ts-node-action/init.lua
+++ lua/ts-node-action/init.lua
@@ -62,11 +62,38 @@ end
 --- @return function|nil
 local function find_action(node, lang)
   local type = node:type()
+
   if M.node_actions[lang] and M.node_actions[lang][type] then
     return M.node_actions[lang][type]
   else
     return M.node_actions["*"][type]
   end
+end
+
+
+local function find_action_function_by_name(node, lang, action_name)
+  local type_ = node:type()
+  local actions = nil
+
+  if M.node_actions[lang] and M.node_actions[lang][type_] then
+    actions = M.node_actions[lang][type_]
+  else
+    actions = M.node_actions["*"][type_]
+  end
+
+  if type(actions) ~= "table" then
+    return nil
+  end
+
+  for _, action in ipairs(actions)
+  do
+    if action.name == action_name
+    then
+      return action[1]
+    end
+  end
+
+  return nil
 end
 
 M.node_actions = require("ts-node-action.filetypes")
@@ -138,6 +165,128 @@ M.node_action = require("ts-node-action.repeat").set(function()
         .. node:type()
         .. "'"
     )
+  end
+end)
+
+--- Find all children that fit within `range`.
+---
+--- Important:
+---     This function is **inclusive**, the ``root`` is returned as the first index.
+---
+--- @param root TSNode A Neovim tree-sitter node to check within.
+--- @param range Range2 The 0-or-more treesitter start/end index lines.
+--- @return TSNode[] # The found children, if any.
+---
+local function _traverse_within_range(root, range)
+  local start, end_ = unpack(range)
+
+  local stack = {root}
+  local output = {}
+
+  while not vim.tbl_isempty(stack)
+  do
+    local current = table.remove(stack)
+
+    table.insert(output, current)
+
+    for child in current:iter_children()
+    do
+      if child:start() <= end_ and child:end_() >= start then
+        -- NOTE: ``child`` is in range
+        table.insert(stack, child)
+      end
+    end
+  end
+
+  return output
+end
+
+--- @class _NodeData
+---     A description of the injected data and its root.
+--- @field node
+---     The starting root node for this injected language.
+--- @field language
+---     The name of the injected language.
+---
+
+--- Find all injected languages in `parser`.
+---
+--- @param parser vim.treesitter.LanguageTree
+---     A starting tree to get all injections / trees.
+--- @return table<string, string>
+---     The found injections, if any.
+---
+local function _initialize_injections(parser)
+  local injections = {} ---@type table<string, vim.treesitter.dev.Injection>
+
+  parser:for_each_tree(function(parent_tree, parent_ltree)
+    local parent = parent_tree:root()
+    for _, child in pairs(parent_ltree:children()) do
+      for _, tree in pairs(child:trees()) do
+        local r = tree:root()
+        local node = assert(parent:named_descendant_for_range(r:range()))
+        local id = node:id()
+        if not injections[id] or r:byte_length() > injections[id].root:byte_length() then
+          injections[id] = child:lang()
+        end
+      end
+    end
+  end)
+
+  return injections
+end
+
+--- Find all nodes within line `range` at `buffer`.
+---
+--- @param range Range2 The 0-or-more treesitter start/end index lines.
+--- @param buffer number? A Vim buffer to query from. 0 == current buffer.
+--- @return _NodeData[] # The found nodes, if any.
+---
+local function _get_nodes_in_range(range, buffer)
+  local buffer = buffer or 0
+  local parser = vim.treesitter.get_parser(buffer)
+  local injections = _initialize_injections(parser)
+  local trees = parser:parse(range)
+  local buffer_language = parser:lang()
+
+  local nodes = {}
+
+  for _, tree in ipairs(trees)
+  do
+    local root = tree:root()
+    local language = injections[root:id()] or buffer_language
+
+    for _, node in ipairs(_traverse_within_range(root, range))
+    do
+      table.insert(nodes, {node=node, language=language})
+    end
+  end
+
+  return nodes
+end
+
+M.run_action_on_visual_lines = require("ts-node-action.repeat").set(function(action_name)
+  local buffer = vim.fn.bufnr()
+  local region = vim.fn.getregionpos(vim.fn.getpos("."), vim.fn.getpos("v"), {type="V"})
+  local vim_start_line = region[1]
+  local vim_end_line = region[2]
+  local treesitter_start_line = vim_start_line - 1
+  local treesitter_end_line = vim_end_line - 1
+
+  local nodes = _get_nodes_in_range(
+    {treesitter_start_line, treesitter_end_line},
+    buffer
+  )
+
+  for _, entry in ipairs(nodes)
+  do
+    local node = entry.node
+    local lang = entry.language
+    local action = find_action_function_by_name(node, lang, action_name)
+
+    if type(action) == "function" then
+      do_action(action, node)
+    end
   end
 end)
 
